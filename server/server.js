62eac53220cd1d0df1f8bebbca9d18e09433bcc7
Raymen Scholten <raymenscholten@gmail.com>
2015-01-24 18:11:24 +0100

    Further implementation of the api. (should) Sign the refundtransaction

--- server/server.js
+++ server/server.js
@@ -11,11 +11,16 @@ app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(multer()); // for parsing multipart/form-data
 
+// I'm alive!
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-// { userPublicKey : hex } : { ServerPublicKey : hex }
+// User requests a public key, required to create: 2of2Address, paymentTransaction, refundTransaction
+// Creating these on the server is not possible (too bad :( ), so there is some effort required from the user.
+// Food of thought: how to minimize the impact on the user? tools? wallet implementations?
+// Input:   	{ userPublicKey : hex }
+// Output: 		{ ServerPublicKey : hex }
 app.post('/wallet/create', function (req, res) {
     var userPubKeyHex = req.body.userPublicKey;
     console.log("Creating key for the user: " + userPubKeyHex);
@@ -37,18 +42,39 @@ app.post('/wallet/create', function (req, res) {
 	res.send({ "ServerPublicKey" : serverPrivKey.pub.toHex() })
 });
 
-// { walletOutputScript : hex, rawRefundTransactionHex, userPublicKey : hex } : { signedRefundTransaction : hex }
+// Check if the refundTransaction is valid
+// Return true if valid, false otherwise
+function verifyRefundTransaction(tx)
+{
+	// check if transaction output is from the first public key
+	// .... if we care?
+	
+	// check if transaction is really about refunding the bitcoins back after a given time to the original address
+	// .... do we even care to what address it's being refunded?
+	
+	// .... more?
+	
+	return true;
+}
+
+// 
+// { walletOutputScript : hex, rawRefundTransactionHex, userPublicKey : hex, redeemScript : hex } : 
+// { signedRefundTransaction : hex }
 app.post('/transaction/signrefund', function (req, res) {
+	// convert the input to internal objects
 	var walletAddress = bitcoin.Address.fromOutputScript(req.body.walletOutputScript);
+	
+	// half signed refundTransaction
 	var refundTransaction = bitcoin.Transaction.fromHex(req.body.rawRefundTransactionHex);
 	var tx = bitcoin.TransactionBuilder.fromTransaction(refundTransaction)
 	
-	var userPubKey = bitcoin.ECPubKey.fromHex(req.body.userPublicKey).toHex();
+	// public key of the user
+	var userPubKey = bitcoin.ECPubKey.fromHex(req.body.userPublicKey);
 	
 	// get our private key from the database
 	var entity;
 	
-	db.find(userPubKey, function (err, doc) { 
+	db.find(userPubKey.toHex(), function (err, doc) { 
 		if (err) {
 			console.log(err);
 		} else {
@@ -56,26 +82,49 @@ app.post('/transaction/signrefund', function (req, res) {
 		}
 	});
 	
+	// Calculate redeemscript to sign the transaction
+	var pubKeys = [ userPubKey, entity.ServerPublicKey ];
+	var redeemScript = bitcoin.scripts.multisigOutput(2, pubKeys);
+	
+	// Not playing fair probably... one of the two public keys do not match
+	if(redeemScript.toHex() != req.body.redeemScript)
+	{
+		throw "Calculated redeemscript is not equal to received redeemscript"
+	}
+	
 	var privateKey = bitcoin.ECKey.fromWIF(entity.ServerPrivateKey);
 	
+	if(!verifyRefundTransaction(tx))
+	{
+		throw "Refund transaction is not valid.";
+	}
+	
 	// use the private key to sign the transaction
-	tx.sign(0, privateKey);
+	tx.sign(0, privateKey, redeemScript);
 	var retTx = tx.build();
 	
 	// return the signed transaction
 	res.send({ "signedRefundTransaction" : retTx.toHex() });
 	
+	// probably best to store this refundTransaction so the user can download it at a later date
+	// as most people lose all their stuff, shit happens...
+	
 	// do the communication with party C
+	// request public key from the seller
+	// create the same stuff as the user did for "us" (the server).
+	// meaning: walletOutputScript, rawRefundTransactionHex, pubKey, redeemScript
 });
 
+// return all database values, test all the things!
 app.get('/db/', function (req, res) {
-	db.find({}, function (err, docs) {   // Callback is optional
-	  //if (err) throw err;
+	db.find({}, function (err, docs) {
+	  if (err) { throw err; }
 	  
 	  res.send((docs));
 	});
 });
 
+// Start server on localhost:3000
 var server = app.listen(3000, function () {
 
   var host = server.address().address
