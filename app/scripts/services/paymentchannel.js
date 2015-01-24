05c64d297533f99d7d9e88a9bcc034c5dc64cdcb
baleato <baleato@gmail.com>
2015-01-24 16:12:53 +0100

    Added PaymentChannel service that will use BitcoinJS

---
+++ app/scripts/services/paymentchannel.js
@@ -0,0 +1,13 @@
+angular.module('scratchApp')
+    .factory('PaymentChannel', [ function() {
+
+        /*
+            BitcoinJS is availabel through the 'Bitcoin' public variable
+         */
+
+        return {
+
+            // Export necessary functions here
+
+        };
+    }]);
